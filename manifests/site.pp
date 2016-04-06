7a7c32847f047090b3640bac4875680825d5668c
root <root@skh015.puppetlabs.vm>
2016-04-06 14:20:40 +0000

    node class

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,7 +38,7 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+node 'skh015.puppetlabs.vm' {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
